a16bdceebf0f1686911cba861beec9c661ecba76
AntonioGibas <you@example.com>
2023-10-05 21:56:28 +0200

    Kreiran KorisnickiUnos.cpp kod za korisnicki unos podataka u varijablu

---
+++ Osnove/KorisnickiUnos.cpp
@@ -0,0 +1,16 @@
+#include <bits\stdc++.h>
+
+//Program koji holda output i izvršavanje sve dok ne poprimi nekakav unos
+
+int main(){
+
+    std::string ime, prezime;
+    std::cout<< "Unesi ime: ";
+    std::cin>> ime;
+    std::cout<< "Unesi prezime: ";
+    std::cin>> prezime;
+
+    std::cout<< ime << ' ' << prezime;
+
+    return 0;
+}
