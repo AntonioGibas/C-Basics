910443c36263889e9551edcad3da3adae535a0dd
AntonioGibas <you@example.com>
2023-10-05 21:36:15 +0200

    Edited Konverzija.cpp

--- Osnove/Konverzija.cpp
+++ Osnove/Konverzija.cpp
@@ -8,14 +8,14 @@ int main(){
 
     //Ukoliko vrijendost 3.14 inicijaliziram kao cijeli broj prikazati će se samo 3
     // int x = 3.14;
-    // No ako inicijaliziram kao auto x = 3.14; C++ će odrediti o kakvoj je varijabli riječ (void u ovom slučaju)
+    // No ako inicijaliziram kao auto x = 3.14; C++ će odrediti o kakvoj je varijabli riječ (double u ovom slučaju)
     // te će dodijeliti taj tip varijable kada se program pokrene
 
     auto x = 3.14;
 
-    char x = 100;
+    auto y = 100;
 
-    std::cout << x << '\n'
+    std::cout << y << '\n';
 
     std::cout << x << '\n';
     return 0;
