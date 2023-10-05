31ef6913585cfe52849262d0a1d55c0cff12900b
AntonioGibas <antonio.gibas6@gmail.com>
2023-10-05 20:52:24 +0200

    Dodan primjer za automatsku konverziju tipa varijable

---
+++ Osnove/Konverzija.cpp
@@ -0,0 +1,19 @@
+#include <bits\stdc++.h>
+
+// Tip Konverzije = konverzija vrijednosti jedne vrste podataka u drugu.
+// Primjer: implicitno u automatsko***
+// veoma važno!
+
+int main(){
+
+    //Ukoliko vrijendost 3.14 inicijaliziram kao cijeli broj prikazati će se samo 3
+    // int x = 3.14;
+    // No ako inicijaliziram kao auto x = 3.14; C++ će odrediti o kakvoj je varijabli riječ (void u ovom slučaju)
+    // te će dodijeliti taj tip varijable kada se program pokrene
+
+    auto x = 3.14;
+
+    std::cout << x << '\n';
+    return 0;
+    
+}
