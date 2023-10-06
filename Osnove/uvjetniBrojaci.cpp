a23d2b7219064307b17bfe22064fd3daed93fe52
Antonio Gibas <agibas@croz.net>
2023-10-06 10:23:06 +0200

    Dodan primjer switch case naredbe

---
+++ Osnove/uvjetniBrojaci.cpp
@@ -0,0 +1,53 @@
+#include <bits\stdc++.h>
+
+int main(){
+
+    int i, j;
+    int mjesec;
+
+    std::cout<< "Unesi mjesec: ";
+    std::cin>> mjesec;
+
+    
+   switch(mjesec){
+    case 1:
+        std::cout<< "Sijecanj";
+        break;
+    case 2:
+        std::cout<< "Veljaca";
+        break;
+    case 3:
+        std::cout<< "Ozujak";
+        break;
+    case 4:
+        std::cout<< "Travanj";
+        break;
+    case 5:
+        std::cout<< "Svibanj";
+        break;
+    case 6:
+        std::cout<< "Lipanj";
+        break;
+    case 7:
+        std::cout<< "Srpanj";
+        break;
+    case 8:
+        std::cout<< "Kolovoz";
+        break;
+    case 9:
+        std::cout<< "Rujan";
+        break;
+    case 10:
+        std::cout<< "Listopad";
+        break;
+    case 11:
+        std::cout<< "Studeni";
+        break;
+    case 12:
+        std::cout<< "Prosinac";
+        break;
+   }
+
+
+    return 0;
+}
