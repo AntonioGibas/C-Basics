5203acd2ed9276be1de4eeb487e97136c776fc31
Antonio Gibas <agibas@croz.net>
2023-10-06 09:37:00 +0200

    Dodan primjer max() funkcije za određivanje većeg broja

---
+++ Osnove/AritmetičkeFunkcije.cpp
@@ -0,0 +1,24 @@
+#include <bits\stdc++.h>
+
+int main(){
+
+//    auto x = 0,y = 0,z = 0;  //testni primjer
+//    auto i = 1,j = 1;
+// for (i = 1; i<=10; i++){
+//     for (j = 1; j <= 10; j++){
+//            x = i,y = j;
+//            z = pow(sqrt(x), 2) + (pow(y, 2));
+//            std::cout<< z << '\n';
+//        }     
+//    }
+
+    double x = 3, y = 4;
+    double z;
+
+    z = std::max(x, y); // provjera koji je broj veći od navedena 2.
+
+    std::cout<< z;
+    
+
+    return 0;
+}
