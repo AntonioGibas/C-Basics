03f9667e07eb96cb3950d0ef099daa5733bb8592
Antonio Gibas <agibas@croz.net>
2023-10-09 09:53:29 +0200

    .

---
+++ Osnove/Vjezba2.cpp
@@ -0,0 +1,33 @@
+#include <bits\stdc++.h>
+
+// Jednostavni progam za pregled unesenog broja.
+
+int main(){
+
+    int uneseniBroj, flag = 1, brojac;
+
+    std::cout<< "Unesi broj: ";
+    std::cin>> uneseniBroj;
+
+    for (brojac = 1; brojac <= uneseniBroj; brojac++){
+
+        if (uneseniBroj % brojac == 0){
+
+            flag = 0;
+            break;
+
+        }
+
+    }
+
+    if (flag = 1){
+
+        std::cout<< "Uneseni broj je prost.";
+
+    }
+    else {
+        std::cout<< "Uneseni broj nije prost.";
+    }
+
+    return 0;
+}
